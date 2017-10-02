c98b48c069fa2bb944071f270d971c9bf0af8d2f
Dennis Andriesse <da.andriesse@few.vu.nl>
2017-10-02 10:01:02 +0200

    Bump version number

--- nucleus.h
+++ nucleus.h
@@ -1,7 +1,7 @@
 #ifndef NUCLEUS_H
 #define NUCLEUS_H
 
-#define NUCLEUS_VERSION  "nucleus disassembler v0.63"
+#define NUCLEUS_VERSION  "nucleus disassembler v0.65"
 #define NUCLEUS_CREDITS  "Copyright (C) 2016, 2017 Dennis Andriesse, Vrije Universiteit Amsterdam"
 
 #endif /* NUCLEUS_H */
