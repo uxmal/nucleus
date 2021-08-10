a73edca09f2ce89b389d4914567851f013735b00
John Källén <johnkal@gmail.com>
2021-08-10 13:56:27 +0200

    Feature: add UnitTests project to solution.

---
+++ UnitTests/CfgTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.UnitTests
+{
+    [TestFixture]
+    public class CfgTests
+    {
+        private Binary bin;
+        private DisasmSection disasm;
+        private CFG cfg;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.bin = new Binary();
+            this.disasm = new DisasmSection();
+        }
+
+        private void Given_X86Code(Action<X86Emitter> generator)
+        {
+            bin.reko_arch = new Reko.Arch.X86.X86ArchitectureFlat32(null, "", new Dictionary<string, object>());
+            var m = new X86Emitter(disasm);
+            generator(m);
+            this.cfg = new CFG();
+            cfg.make_cfg(bin, new List<DisasmSection> { disasm });
+        }
+
+        [Test]
+        public void Cfg_Linear()
+        {
+            Given_X86Code(m =>
+            {
+                m.mov(m.eax, m.W32(0x1234));
+                m.ret();
+            });
+
+            Assert.AreEqual(1, cfg.functions.Count);
+        }
+
+        [Test]
+        public void Cfg_Two_Procedures()
+        {
+            Given_X86Code(m =>
+            {
+                m.mov(m.eax, m.W32(0x1234));
+                m.ret();
+                m.mov(m.eax, m.W32(0x5678));
+                m.ret();
+            });
+
+            Assert.AreEqual(2, cfg.functions.Count);
+        }
+
+        [Test]
+        [Ignore("This exposes a flaw in Nucleus out of the box")]
+        public void Cfg_branch_to_procedure()
+        {
+            Given_X86Code(m =>
+            {
+                m.mov(m.eax, m.W32(0x1234));
+                m.call("fn");
+                m.mov(m.eax, m.W32(0x5678));
+
+                m.label("fn");
+                m.mov(m.eax, m.W32(0x5555));
+                m.ret();
+            });
+
+            Assert.AreEqual(2, cfg.functions.Count);
+            var fn1 = cfg.functions[0];
+            Assert.AreEqual(2, fn1.BBs.Count);
+        }
+    }
+}
---
+++ UnitTests/X86Emitter.cs
@@ -0,0 +1,127 @@
+﻿using Reko.Arch.X86;
+using Reko.Core;
+using Reko.Core.Expressions;
+using Reko.Core.Machine;
+using Reko.Core.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.UnitTests
+{
+    public class X86Emitter
+    {
+        private DisasmSection disasm;
+        private BB bb;
+        private Address addr;
+        private Section section;
+        private Dictionary<Address, MachineInstruction> instrs;
+        private Dictionary<string, EmitterSymbol> symbols;
+
+        public X86Emitter(DisasmSection disasm)
+        {
+            this.disasm = disasm;
+            this.addr = Address.Ptr32(0x0010_0000);
+            this.section = new Section();
+            this.section.type = SectionType.CODE;
+            this.symbols = new();
+            this.instrs = new();
+            StartBlock();
+        }
+
+        private void StartBlock()
+        {
+            this.bb = new BB();
+            this.disasm.BBs.Add(bb);
+            this.bb.start = addr.ToLinear();
+            this.bb.section = section;
+        }
+
+        private void EndBlock()
+        {
+            this.bb = null;
+        }
+
+        public RegisterOperand eax { get; } = new RegisterOperand(Registers.eax);
+
+        public void label(string label)
+        {
+            DefineSymbol(label);
+        }
+
+
+        public void call(string label)
+        {
+            var target = EnsureSymbolOperand(label, 0);
+            var instr = new X86Instruction(Mnemonic.call, InstrClass.Transfer | InstrClass.Call, null, PrimitiveType.Ptr32, target);
+            Emit(instr, 5);
+            EndBlock();
+        }
+
+        public void mov(MachineOperand dst, MachineOperand src)
+        {
+            var instr = new X86Instruction(Mnemonic.mov, InstrClass.Linear, dst.Width, PrimitiveType.Ptr32, dst, src);
+            Emit(instr, 2 + (src is MemoryOperand ? 4 : 1));
+        }
+
+        public void ret()
+        {
+            var instr = new X86Instruction(Mnemonic.ret, InstrClass.Transfer|InstrClass.Return, null, PrimitiveType.Ptr32);
+            Emit(instr, 1);
+            EndBlock();
+        }
+
+
+        private AddressOperand EnsureSymbolOperand(string label, int iop)
+        {
+            if (symbols.TryGetValue(label, out var symbol) && symbol.resolved)
+            {
+                return AddressOperand.Create(symbol.addr);
+            }
+            symbol = new EmitterSymbol();
+            symbol.backpatches.Add((this.addr, iop));
+            symbols[label] = symbol;
+            return AddressOperand.Create(Address.Ptr64(~0u));
+        }
+
+        private void DefineSymbol(string label)
+        {
+            if (symbols.TryGetValue(label, out var symbol))
+            {
+                foreach (var (addr, iop) in symbol.backpatches)
+                {
+                    instrs[addr].Operands[iop] = AddressOperand.Create(this.addr);
+                }
+            }
+            var sym = new EmitterSymbol { addr = this.addr, resolved = true };
+            symbols[label] = sym;
+        }
+
+        public MemoryOperand W32(uint offset)
+        {
+            var w = PrimitiveType.Word32;
+            return new MemoryOperand(w, Constant.Create(PrimitiveType.Word32, offset));
+        }
+
+        protected void Emit(X86Instruction instr, int length)
+        {
+            if (bb is null)
+                StartBlock();
+
+            instr.Length = length;
+            instr.Address = this.addr;
+            this.instrs[addr] = instr;
+            bb.insns.Add(instr);
+            this.addr += length;
+        }
+
+        class EmitterSymbol
+        {
+            public Address addr;
+            public bool resolved;
+            public List<(Address, int)> backpatches = new();
+        }
+    }
+}
